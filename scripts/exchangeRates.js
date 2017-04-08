e4633bac31e48ee3e6ac8756d07dad90736a9503
Toby <Tobytee17@gmail.com>
2017-04-08 13:36:17 +1200

    Added date to output + groundwork for input validation

--- scripts/exchangeRates.js
+++ scripts/exchangeRates.js
@@ -1,18 +1,29 @@
 var currentRates
+var date
 
 function getRates() {
     $.getJSON("http://api.fixer.io/latest?base=USD", function(data){
         currentRates = data.rates
+        date = data.date
         console.log(currentRates);
+        console.log(date);
     });
 }
 
 getRates();
 
 function convertCurrency() {
+  //Validates the user input amount to make sure it is an integer
+    var myRegEx = /^-?\d+\.?\d*$/
+    var regExFlag = myRegEx.test(document.getElementById('userInput').value)
+    alert(regExFlag);
+
+
+
+//Gets the two different user selected currencies
     var currency1 = document.getElementById('Currency1').value
     var currency2 = document.getElementById('Currency2').value
-
+//Matches user selected currencies to the values sent by the API
     for(var x in currentRates) {
         if(x = currency1) {
             var currency1Value = currentRates[x]
@@ -22,11 +33,14 @@ function convertCurrency() {
             }
         }
     }
+
+
     var currencyAmount = document.getElementById('userInput').value
 
     var result = (1/currency1Value) * currencyAmount * currency2Value
 
     document.getElementById('resultsArea').innerHTML = currencyAmount + " " + currency1 + " = " + result.toFixed(3) + " " + currency2
+    document.getElementById('dateArea').innerHTML = "As of " + date + " 4pm CET"
     console.log(result)
     //console.log(currency1Value + " " + currency2Value)
     //console.log(currencyAmount)
