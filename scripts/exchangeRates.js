11cccd0ed4c5ae696e82f752c3a19e251c9538ac
Tobytee17 <Tobytee17@gmail.com>
2017-04-09 15:38:39 +1200

    cleaned up scripts, recoloured page

--- scripts/exchangeRates.js
+++ scripts/exchangeRates.js
@@ -2,14 +2,13 @@ var currentRates
 var date
 
 function getRates() {
-    $.getJSON("http://api.fixer.io/latest?base=USD", function(data){
+    $.getJSON("http://api.fixer.io/latest?base=USD", function(data) {
         currentRates = data.rates
         date = data.date
         console.log(currentRates);
         console.log(date);
     });
 }
-
 getRates();
 
 function convertCurrency() {
@@ -17,39 +16,36 @@ function convertCurrency() {
     var myRegEx = /^-?\d+\.?\d*$/
     var regExFlag = myRegEx.test(document.getElementById('userInput').value)
     //if input is invalid, reset all text areas + give error message
-    if (regExFlag === false) {
-        alert("Please enter a valid value")
-        document.getElementById('userInput').value = ""
-        document.getElementById('resultsArea').innerHTML = ""
-        document.getElementById('dateArea').innerHTML = ""
-    } else if (regExFlag === true) {
-        //Gets the two different user selected currencies
-        var currency1 = document.getElementById('Currency1').value
-        var currency2 = document.getElementById('Currency2').value
-        //Matches user selected currencies to the values sent by the API
-        for (var x in currentRates) {
-            if (x = currency1) {
-                var currency1Value = currentRates[x]
-                if (x = currency2) {
-                    var currency2Value = currentRates[x]
-                    break
+    if ((document.getElementById('Currency1').value == "") || document.getElementById('Currency2').value == "") {
+        alert("Please Select two currencies")
+    } else {
+        if (regExFlag === false) {
+            alert("Please enter a valid amount")
+            document.getElementById('userInput').value = ""
+            document.getElementById('resultsArea').innerHTML = ""
+            document.getElementById('dateArea').innerHTML = ""
+        } else if (regExFlag === true) {
+            //Gets the two different user selected currencies
+            var currency1 = document.getElementById('Currency1').value
+            var currency2 = document.getElementById('Currency2').value
+
+            //Matches user selected currencies to the values sent by the API
+            for (var x in currentRates) {
+                if (x = currency1) {
+                    var currency1Value = currentRates[x]
+                    if (x = currency2) {
+                        var currency2Value = currentRates[x]
+                        break
+                    }
                 }
             }
+            //conversion equation + display result
+            var currencyAmount = document.getElementById('userInput').value
+            var result = (1 / currency1Value) * currencyAmount * currency2Value
+            document.getElementById('result').innerHTML = result.toFixed(3) + " " + currency2
+            document.getElementById('conversionOutput').innerHTML = " = " + currencyAmount + " " + currency1
+            document.getElementById('dateArea').innerHTML = "As of " + date + " 4pm CET"
+            console.log(result)
         }
-        //conversion equation + display result
-        var currencyAmount = document.getElementById('userInput').value
-        var result = (1 / currency1Value) * currencyAmount * currency2Value
-        document.getElementById('resultsArea').innerHTML = currencyAmount + " " + currency1 + " = " + result.toFixed(3) + " " + currency2
-        document.getElementById('dateArea').innerHTML = "As of " + date + " 4pm CET"
-        console.log(result)
     }
-
-
-
-
-
-
-    //console.log(currency1Value + " " + currency2Value)
-    //console.log(currencyAmount)
-    //console.log(currency1 + " " + currency2)
 }
